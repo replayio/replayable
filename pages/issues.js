1e223ffbc3c41abe887cd0814b2cdbd3a50a5bb3
Cecelia Martinez <ceceliacreates@gmail.com>
2022-04-28 08:26:42 -0400

    github query with example repo

--- pages/issues.js
+++ pages/issues.js
@@ -2,15 +2,45 @@ function Issues({issues}) {
     return (
         <ul>
             {issues.map((issue) => (
-                <li>{issue.title}</li>
+                <li>{issue.node.title}</li>
             ))}
         </ul>
     )
 }
 
     export async function getStaticProps() {
-        const res = await fetch('http://localhost:3000/api/issues')
-        const issues = await res.json()
+        const res = await fetch('https://api.github.com/graphql', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `bearer ${process.env.GITHUB_API_TOKEN}`
+            },
+            body: JSON.stringify({
+                query: `
+                query getIssues {
+                    repository(owner:"octocat", name:"Hello-World") {
+                        issues(last:20, states:CLOSED) {
+                          edges {
+                            node {
+                              title
+                              url
+                              labels(first:5) {
+                                edges {
+                                  node {
+                                    name
+                                  }
+                                }
+                              }
+                            }
+                          }
+                        }
+                      }
+                }`,
+                variables: {}
+            })
+        })
+        const data = await res.json()
+        const issues = data.data.repository.issues.edges
 
         return {
             props: {
