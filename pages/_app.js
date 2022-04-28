e9f9eba4b9fb21df3ac44cbfa23c3efd1c16eb0f
Cecelia Martinez <ceceliacreates@gmail.com>
2022-04-28 07:17:38 -0400

    updates head tag and creates issues page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+        <title>got bugs</title>
+        <meta name="description" content="got bugs?" />
+        <link rel="icon" href="/question-mark.svg" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- pages/issues.js
+++ pages/issues.js
@@ -1,11 +1,10 @@
-
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-            <main className={styles.main}>
-        <h1>got bugs?</h1>
+      <main className={styles.main}>
+        <h1>Issues</h1>
       </main>
     </div>
   );
