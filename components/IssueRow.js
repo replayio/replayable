918f2a8fb4322ef9722838f21aae85a81c3811b8
Jason Laster <jason.laster.11@gmail.com>
2022-04-30 09:14:40 -0700

    Updates

--- components/IssueRow.js
+++ components/IssueRow.js
@@ -1,19 +1,83 @@
-import IssueData from "../components/IssueData";
+import Image from "next/image";
 import IssueLinks from "./IssueLinks";
 import styles from "../styles/Home.module.css";
+import { getLabelFontColor } from "../scripts/utilities";
 
-export default function IssueRow({ issue }) {
+export default function IssueRow({ issue, toggleLabel }) {
+  const title = issue.title;
+  const date = issue.createdAt;
+  const author = issue.author;
+  const number = issue.number;
+  const url = issue.url;
+  const labels = issue.labels.edges;
+  const formattedDate = new Date(date).toLocaleDateString();
+
   return (
     <div className={styles.row}>
-      <IssueData
-        title={issue.title}
-        date={issue.createdAt}
-        author={issue.author}
-        number={issue.number}
-        url={issue.url}
-        labels={issue.labels.edges}
-      />
+      {/* Left Side Issue Summary */}
+      <div className={styles.issue}>
+        {/* Avatar */}
+        <div className={styles.avatarContainer}>
+          <Image
+            className={styles.avatar}
+            src={author.avatarUrl}
+            alt="Picture of the author"
+            layout="fixed"
+            height={30}
+            width={30}
+          />
+        </div>
+
+        <div className={styles.data}>
+          {/* Title and Labels */}
+          <div>
+            <a href={url} target="_blank" rel="noreferrer">
+              <span>{title}</span>
+            </a>
+            <div className={styles.labels}>
+              {labels
+                .filter((label) => label.node.name != "has-replay 🚀")
+                .map((label) => (
+                  <Label
+                    key={label.node.name}
+                    label={label}
+                    toggleLabel={toggleLabel}
+                  />
+                ))}
+            </div>
+          </div>
+
+          {/* Secondary summary row */}
+          <div className={styles.muted}>
+            <span>Issue #{number}</span> <span>Created on {formattedDate}</span>{" "}
+            <span>
+              by{" "}
+              <a href={author.url} target="_blank" rel="noreferrer">
+                {author.name}
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
+
       <IssueLinks issue={issue} />
     </div>
+  );
+}
+
+function Label({ label, toggleLabel }) {
+  const fontColor = getLabelFontColor(label.node.color);
+
+  return (
+    <span
+      className={styles.label}
+      onClick={() => toggleLabel(label.node.name)}
+      style={{
+        backgroundColor: "#" + label.node.color,
+        color: fontColor,
+      }}
+    >
+      {label.node.name}
+    </span>
   );
 }
--- pages/api/issues.js
+++ pages/api/issues.js
@@ -1,7 +1,68 @@
-import issues from '../../data/issues.json'
-
-export default function handler(req, res) {
-    res.status(200).json(issues)
+const query = `
+query getIssues(
+  $labels: [String!], 
+  $owner: String!, 
+  $repo: String!
+) {
+  repository(owner: $owner, name: $repo) {
+    issues(last: 50, states: OPEN, labels: $labels) {
+      edges {
+        node {
+          title
+          url
+          labels(first: 5) {
+            edges {
+              node {
+                name
+                color
+              }
+            }
+          }
+          body
+          author {
+            ... on User {
+              avatarUrl
+              name
+              url
+            }
+          }
+          number
+          createdAt
+        }
+      }
+    }
   }
+}`;
 
-  
+export default async function handler(req, res) {
+  const variables = {
+    ...req.query,
+    labels: req.query.labels?.split(",") || "",
+  };
+
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `bearer ${process.env.GITHUB_API_TOKEN}`,
+  };
+
+  console.log(
+    "...\n",
+    query,
+    "\n\n",
+    JSON.stringify(variables, null, 2),
+    "\nheaders\n",
+    JSON.stringify(headers, null, 2)
+  );
+  const response = await fetch("https://api.github.com/graphql", {
+    method: "POST",
+    headers,
+    body: JSON.stringify({
+      query,
+      variables: req.query,
+    }),
+  });
+  const data = await response.json();
+  const issues = data.data.repository.issues.edges;
+  console.log("issues", issues);
+  return res.json(issues);
+}
