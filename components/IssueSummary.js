2214d608e669d19914ec303ec7c22aedde77ed3c
Jason Laster <jason.laster.11@gmail.com>
2022-04-30 20:12:11 -0700

    Support search api

--- components/IssueSummary.js
+++ components/IssueSummary.js
@@ -1,42 +1,58 @@
 import styles from "../styles/Home.module.css";
 
-export default function IssueSummary({ issues, filters, toggleIssueState }) {
+export default function IssueSummary({
+  issues,
+  filters,
+  toggleRepo,
+  toggleIssueState,
+}) {
   const { state, labels } = filters;
   return (
-    <div className={styles.tableHeader}>
-      <div>
-        <span
-          className={styles.headerLabel}
-          style={
-            state === "OPEN"
-              ? { fontWeight: "bold" }
-              : { textDecoration: "underline", cursor: "pointer" }
-          }
-          onClick={() => toggleIssueState("OPEN")}
-        >
-          {state === "OPEN" ? issues.length + " " : null}
-          {state === "OPEN" ? "Open" : "View Open"}
-        </span>
-        <span
-          className={styles.headerLabel}
-          style={
-            state === "CLOSED"
-              ? { fontWeight: "bold" }
-              : { textDecoration: "underline", cursor: "pointer" }
-          }
-          onClick={() => toggleIssueState("CLOSED")}
-        >
-          {state === "CLOSED" ? issues.length + " " : null}
-          {state === "OPEN" ? "View Closed" : "Closed"}
-        </span>
-      </div>
-      <div>
-        {labels.map((label) => (
-          <span className={styles.headerLabel} key={label}>
-            {label}
+    <>
+      {filters.repo && (
+        <h2>
+          {filters.org}/{filters.repo}
+          <a href="#" onClick={() => toggleRepo("", "")}>
+            {" "}
+            x
+          </a>
+        </h2>
+      )}
+      <div className={styles.tableHeader}>
+        <div>
+          <span
+            className={styles.headerLabel}
+            style={
+              state === "OPEN"
+                ? { fontWeight: "bold" }
+                : { textDecoration: "underline", cursor: "pointer" }
+            }
+            onClick={() => toggleIssueState("OPEN")}
+          >
+            {state === "OPEN" ? issues.length + " " : null}
+            {state === "OPEN" ? "Open" : "View Open"}
           </span>
-        ))}
+          <span
+            className={styles.headerLabel}
+            style={
+              state === "CLOSED"
+                ? { fontWeight: "bold" }
+                : { textDecoration: "underline", cursor: "pointer" }
+            }
+            onClick={() => toggleIssueState("CLOSED")}
+          >
+            {state === "CLOSED" ? issues.length + " " : null}
+            {state === "OPEN" ? "View Closed" : "Closed"}
+          </span>
+        </div>
+        <div>
+          {labels.map((label) => (
+            <span className={styles.headerLabel} key={label}>
+              {label}
+            </span>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
---
+++ pages/api/search.js
@@ -0,0 +1,70 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+const query = `
+query($query: String!) { 
+  search(query:$query, type:ISSUE, first: 100) {
+		issueCount
+		nodes {
+      ... on Issue {
+        title
+        url
+        body
+        number
+        createdAt
+        repository {
+          name
+          url
+          owner {
+            login
+          }
+        }
+        author {
+          ... on User {
+            avatarUrl
+            name
+            url
+          }
+        }
+        labels(first: 5) {
+          edges {
+            node {
+              name
+              color
+            }
+          }
+        }
+      }
+    }
+  }
+}`;
+
+export default async function handler(req, res) {
+  console.log(req.query);
+
+  const { repo, org, state, labels } = req.query;
+  const repoQuery = repo != "" ? `repo:${org}/${repo}` : "";
+  const labelQuery = labels
+    .split(",")
+    .map((label) => `label:"${label}"`)
+    .join(" ");
+  const variables = {
+    query: `${labelQuery} ${repoQuery} is:${state}`,
+  };
+
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `bearer ${process.env.GITHUB_API_TOKEN}`,
+  };
+  console.log(variables);
+  const response = await fetch("https://api.github.com/graphql", {
+    method: "POST",
+    headers,
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  });
+  const data = await response.json();
+  const issues = data.data.search.nodes;
+  return res.json(issues);
+}
