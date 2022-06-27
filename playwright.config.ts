71624fef197138fa5a99596ca1f2a16396bb6d4f
Cecelia Martinez <ceceliacreates@gmail.com>
2022-06-27 11:24:50 -0400

    revert test change

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,6 +11,7 @@ if (!inCI) {
 
 const config: PlaywrightTestConfig = {
   timeout: !inCI ? 30 * 1000 : 10 * 1000,
+  workers: !inCI ? 1 : undefined,
   use: {
     baseURL: !inCI ? "http://localhost:3000" : undefined,
     launchOptions: {
