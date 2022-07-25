41b06828a172e4d0c6b150fd6776d39f6ccd6281
Ryan Duffy <ryan@replay.io>
2022-07-25 16:02:35 -0700

    global install

--- scripts/cypress-replay.sh
+++ scripts/cypress-replay.sh
@@ -10,15 +10,11 @@ export RECORD_REPLAY_METADATA_FILE=$(mktemp)
 export RECORD_ALL_CONTENT=1
 export RECORD_REPLAY_API_SERVER=${RECORD_REPLAY_API_SERVER:-https://api.replay.io}
 
-CWD=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd );
-
 # Install dependencies for this script
-npm i --prefix $CWD @replayio/replay@0.0.0-experimental-2640f652
+npm i -g @replayio/replay@0.0.0-experimental-2640f652
 
 # Start Dev Server or configure BASE_URL and run your tests
 eval ${TEST_COMMAND} || true
-
-cd $CWD
 
 replay metadata --init --keys source --warn
 replay upload-all
