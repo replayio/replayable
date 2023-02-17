f902870c643ccf64c98dfe97bf2fbbfadcd72171
Tejas Kumar <tejas@tejas.qa>
2023-02-17 14:42:49 +0100

    Prettier

--- util/filtersToQuery.ts
+++ util/filtersToQuery.ts
@@ -1,7 +1,5 @@
-
-
 export function filtersToQuery(filters) {
-    return Object.keys(filters)
-        .map((key) => key + "=" + encodeURIComponent(filters[key] || ""))
-        .join("&");
+  return Object.keys(filters)
+    .map((key) => key + "=" + encodeURIComponent(filters[key] || ""))
+    .join("&");
 }
