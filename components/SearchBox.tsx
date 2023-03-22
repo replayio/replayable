7cdaa71e799f1dce038c8a2ec1d0d9082356af18
Shreya Ghosh <sgshreya024@gmail.com>
2023-03-22 21:51:27 +0530

    API resolved without sending a response

--- components/SearchBox.tsx
+++ components/SearchBox.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import styles from "../styles/search.module.css";
+import { useRouter } from "next/router";
 
-const SearchBox = () => {
-  const submitHandler = (e) => {
+export default function SearchBox() {
+  const router = useRouter();
+
+  function submitHandler(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-  };
+    console.log(e.target[0].value);
+    router.push(`/api/search?q=${e.target[0].value}`);
+  }
+
   return (
     <form onSubmit={submitHandler} className={styles.searchbar}>
-      <input type="search" name="search" pattern=".*\S.*" required />
+      <input type="search" name="query" pattern=".*\S.*" required />
       <button className={styles.searchbtn} type="submit">
         <span>Search</span>
       </button>
     </form>
   );
-};
-
-export default SearchBox;
+}
